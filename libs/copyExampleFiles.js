42fa22184b00a6ee53f209fd8b4c96a9a5b61dfe
Simons-Maddy <xiaochunfu@126.com>
2022-12-03 13:49:32 +0800

    update example files copy function

--- libs/copyExampleFiles.js
+++ libs/copyExampleFiles.js
@@ -7,10 +7,10 @@ export default function copyExampleFiles(matrixFilePath,resultPath){
         let exampleFilesPath = matrixFilePath.substring(0, matrixFilePath.lastIndexOf('/') + 1)
         // resultPath end without "/"
         // copy example files
-        fs.copyFileSync(exampleFilesPath+"sc.h5ad",resultPath)
-        fs.copyFileSync(exampleFilesPath+"sc_markers.json",resultPath+"/out/json/")
-        fs.copyFileSync(exampleFilesPath+"meta_info.preprocessing.json",resultPath+"/out/json/")
-        fs.copyFileSync(exampleFilesPath+"filter_summary.preprocessing.json",resultPath+"/out/json/")
+        fs.copyFileSync(exampleFilesPath+"sc.h5ad",resultPath+"/sc.h5ad")
+        fs.copyFileSync(exampleFilesPath+"sc_markers.json",resultPath+"/out/json/sc_markers.json")
+        fs.copyFileSync(exampleFilesPath+"meta_info.preprocessing.json",resultPath+"/out/json/meta_info.preprocessing.json")
+        fs.copyFileSync(exampleFilesPath+"filter_summary.preprocessing.json",resultPath+"/out/json/filter_summary.preprocessing.json")
     }catch (e){
         annotationLogger.log(`[${new Date().toLocaleString()}] Error: Copy example files failed: ${e}`)
     }
